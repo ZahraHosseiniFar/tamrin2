d68da25e40b0e8c8c3891c05b7cdcc08d673ee75
ZahraHosseiniFar <117304346+ZahraHosseiniFar@users.noreply.github.com>
2023-01-27 19:35:08 +0330

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Form  A = new Form();
+            A.Name = "ZAHRA  ";
+            A.Family = "HOSSEINIFAR  ";
+            A.Id = 123456789  ;
+            A.Time = 8  ;
+            A.Pay = 50000  ;
+            A.workre();
+            Console.WriteLine(Convert.ToString(A.payment()));
+            Console.ReadKey();
+        }
+    }
+}
